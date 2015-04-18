ffeaf58f3d0b68d217265fb88be6af10f9a085ef
= <=>
2015-04-18 01:16:39 -0400

    Finish fix for 1.9.3

--- lib/watir-ng.rb
+++ lib/watir-ng.rb
@@ -11,24 +11,36 @@ module WatirNg
   #
   # @see `https://docs.angularjs.org/api/ng/directive`
   #
-  NG_DIRECTIVES = %w(ng_jq ng_app ng_href ng_src ng_srcset ng_disabled ng_checked ng_readonly 
-                     ng_selected ng_open ng_form ng_value ng_bind ng_bind_template ng_bind_html 
-                     ng_change ng_class ng_class_odd ng_class_even ng_cloak ng_controller ng_csp 
-                     ng_click ng_dblclick ng_mousedown ng_mouseup ng_mouseover ng_mouseenter 
-                     ng_mouseleave ng_mousemove ng_keydown ng_keyup ng_keypress ng_submit ng_focus
-                     ng_blur ng_copy ng_cut ng_paste ng_if ng_include ng_init ng_list ng_model 
-                     ng_model_options ng_non_bindable ng_options ng_pluralize ng_repeat ng_show 
-                     ng_hide ng_style ng_switch ng_transclude
-                    )
+  NG_STRINGS = %w(ng_jq ng_app ng_href ng_src ng_srcset ng_disabled ng_checked ng_readonly 
+                  ng_selected ng_open ng_form ng_value ng_bind ng_bind_template ng_bind_html 
+                  ng_change ng_class ng_class_odd ng_class_even ng_cloak ng_controller ng_csp 
+                  ng_click ng_dblclick ng_mousedown ng_mouseup ng_mouseover ng_mouseenter 
+                  ng_mouseleave ng_mousemove ng_keydown ng_keyup ng_keypress ng_submit ng_focus
+                  ng_blur ng_copy ng_cut ng_paste ng_if ng_include ng_init ng_list ng_model 
+                  ng_model_options ng_non_bindable ng_options ng_pluralize ng_repeat ng_show 
+                  ng_hide ng_style ng_switch ng_transclude
+                 )
 
   #
-  # Inserts `NG_DIRECTIVES` into `cls.attributes` when included in the class.
+  # Inserts `NG_STRINGS` into `cls.attributes` as symbols when included in the class.
   #
   # @param cls [Class]
   # @return [nil]
   #
   def self.included cls
-    NG_DIRECTIVES.map(&:to_sym).each { |ng| cls.attributes << ng } if cls.respond_to? :attributes
+    if cls.respond_to? :attributes
+      ng_directives.each { |ng| cls.attributes << ng }
+    end
+  end
+
+  #
+  # Converts `NG_STRINGS` into an array of symbols.
+  #
+  # @api private
+  # @return [Array<Symbol>] directives
+  #
+  def self.ng_directives
+    @ng_directives ||= NG_STRINGS.map(&:to_sym)
   end
 end
 
--- spec/watir_ng_spec.rb
+++ spec/watir_ng_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe WatirNg do
+  let(:directives){ WatirNg::NG_STRINGS.map(&:to_sym) }
+  
   it 'has a version number' do
     expect(WatirNg::VERSION).not_to be nil
   end
@@ -8,13 +10,13 @@ describe WatirNg do
   context "when included in a class" do
     it "adds each ng directive to class.attributes" do
       TestClass.send(:include, WatirNg)
-      expect(TestClass.attributes).to eq WatirNg::NG_DIRECTIVES
+      expect(TestClass.attributes).to eq directives
     end
 
     it "does not overwrite the class.attributes" do
       TestClass.attributes = [:foo]
       TestClass.send(:include, WatirNg)
-      expect(TestClass.attributes).to eq [:foo] + WatirNg::NG_DIRECTIVES
+      expect(TestClass.attributes).to eq [:foo] + directives
     end
   end
 end

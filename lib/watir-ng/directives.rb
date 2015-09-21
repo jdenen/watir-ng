84a044a3c7a5fe8ec077c72d0b18909460493737
Johnson Denen <johnson.denen@gmail.com>
2015-09-21 17:01:30 -0400

    Add docs to new class and its methods [ci-skip]

--- lib/watir-ng/directives.rb
+++ lib/watir-ng/directives.rb
@@ -1,5 +1,15 @@
 module WatirNg
+  
+  #
+  # Object that stores ng directives and patches custom directives onto Watir::HTMLElement.
+  #
   class Directives
+    
+    #
+    # Returns array of ng directives.
+    #
+    # @return [Array<String>]
+    #
     def ng
       %w(ng_jq ng_app ng_href ng_src ng_srcset ng_disabled ng_checked ng_readonly 
          ng_selected ng_open ng_form ng_value ng_bind ng_bind_template ng_bind_html 
@@ -10,7 +20,15 @@ module WatirNg
          ng_model_options ng_non_bindable ng_options ng_pluralize ng_repeat ng_show 
          ng_hide ng_style ng_switch ng_transclude)
     end
-
+    
+    #
+    # Patches each element in an array onto `Watir::HTMLElement.attributes`.
+    #
+    # @param directives [Array] identifiers to be patched onto HTMLElement
+    # @return [Array]
+    #
+    # @see Watir::HTMLElement
+    #
     def add directives
       directives.tap do |array|
         array.each { |custom| Watir::HTMLElement.attributes << custom }
